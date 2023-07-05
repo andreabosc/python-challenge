8fd1300b9f687f7969d40dff541970a0b0d966e4
andreabosc <131720819+andreabosc@users.noreply.github.com>
2023-07-05 16:04:45 -0400

    Update main.py

--- PyBank/main.py
+++ PyBank/main.py
@@ -1,1 +1,13 @@
+import csv
+with open('budget_data.csv','r') as csvfile:
+  reader_variable = 
+  csv.reader(csvfile for row in reader_variable:
+  print(row)
 
+import calendar
+#get all months name using number
+for i in range(1,13):
+  #get month name
+  print(month
+
+calendar.month_name[num]
